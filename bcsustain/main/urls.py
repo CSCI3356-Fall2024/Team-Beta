17dda3e8ba0edb861fad88c4148ccc42c9dbf2ff
All-money <almanybert@gmail.com>
2024-10-21 23:17:00 -0400

    Campaign form creation

--- bcsustain/main/urls.py
+++ bcsustain/main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signup, login, rewards, supervisorrewards, add_reward, action, base, supervisor, landing, profile_setup
+from .views import signup, login, rewards, supervisorrewards, add_reward, action, base, supervisor, landing, campaign_form, profile_setup
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
@@ -11,5 +11,6 @@ urlpatterns = [
     path('supervisor/', supervisor, name = 'supervisor'),
     path('base/', base, name = 'base'),
     path('landing/', landing, name = 'landing'),
+    path('campaign_form/', campaign_form, name = 'campaign_form'),
     path('profile_setup/', profile_setup, name = 'profile_setup'),
 ]
