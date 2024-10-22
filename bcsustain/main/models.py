16dc515d88e855d1e4daecea05a443e92b405cfa
jacobhsi2h <hsiehjg@bc.edu>
2024-10-21 22:57:09 -0400

    auth_user for manage-supervisor

--- bcsustain/main/models.py
+++ bcsustain/main/models.py
@@ -7,21 +7,21 @@ class CustomUser(AbstractUser):
     is_supervisor = models.BooleanField(default=False)
 
     # Add related_name to avoid clashes with auth.User
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='customuser_set',  # This avoids the clash
-        blank=True,
-        help_text='The groups this user belongs to.',
-        verbose_name='groups'
-    )
+    # groups = models.ManyToManyField(
+    #     'auth.Group',
+    #     related_name='customuser_set',  # This avoids the clash
+    #     blank=True,
+    #     help_text='The groups this user belongs to.',
+    #     verbose_name='groups'
+    # )
     
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='customuser_set_permissions',  # This avoids the clash
-        blank=True,
-        help_text='Specific permissions for this user.',
-        verbose_name='user permissions'
-    )
+    # user_permissions = models.ManyToManyField(
+    #     'auth.Permission',
+    #     related_name='customuser_set_permissions',  # This avoids the clash
+    #     blank=True,
+    #     help_text='Specific permissions for this user.',
+    #     verbose_name='user permissions'
+    # )
 
 class Campaign(models.Model):
     name = models.CharField(max_length=255)
