a29a9041c9108fa1eb793310c174191a5beaf5bd
jacobhsi2h <hsiehjg@bc.edu>
2024-10-21 21:47:25 -0400

    fixing admin

--- bcsustain/main/admin.py
+++ bcsustain/main/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 from .models import Campaign
 
-<<<<<<< HEAD
 # Register your models here.
-=======
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('is_supervisor',)}),
+    )
+
 @admin.register(Campaign)
 class CampaignAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_date', 'end_date', 'location', 'is_active')  # Customize what is shown in the admin list
->>>>>>> 00c3447b7542e9af483689653449f6ca6d1fa11a
