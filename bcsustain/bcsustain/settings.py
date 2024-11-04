90a7fe61ad45a9cd3055220be516e04820a83363
omeryurekli <omeryurekli2004@gmail.com>
2024-11-04 13:25:00 -0500

    Update settings.py

--- bcsustain/bcsustain/settings.py
+++ bcsustain/bcsustain/settings.py
@@ -128,6 +128,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = "598151765783-hlmof392vhfsdorjsbva5dvt1qg0ikkb.apps.googleusercontent.com"
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "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"
 SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI = "http://localhost:8000/complete/google-oauth2/"
+SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI = "http://127.0.0.1:8000/complete/google-oauth2/"
 SOCIAL_AUTH_GOOGLE_OAUTH2_WHITELISTED_DOMAINS = ['bc.edu']
 
 LOGIN_URL = '/login/'
@@ -141,7 +142,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
-    'main.pipeline.ensure_profile_completion',  # Custom step for profile completion
+    'main.pipeline.ensure_profile_completion',  #profile completion pipeline
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
