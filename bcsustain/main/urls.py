f3354c6d5325bc3daad9c57fce0d887a925e6e15
omeryurekli <omeryurekli2004@gmail.com>
2024-11-20 15:06:01 -0500

    trying to fix Google Auth with what Professor samary gave us

--- bcsustain/main/urls.py
+++ bcsustain/main/urls.py
@@ -1,22 +1,29 @@
 from django.urls import path, include
-from .views import signup, login, rewards, supervisorrewards, add_reward, action, base, supervisor, landing, campaign_form, profile_setup, manage_supervisors, delete_campaign
+from .views import (
+    signup, login, rewards, supervisorrewards, add_reward, action, base, 
+    supervisor, landing, campaign_form, profile_setup, manage_supervisors, 
+    delete_campaign
+)
 from django.contrib.auth.views import LogoutView
+from django.contrib import admin
 
 urlpatterns = [
-    path('',login, name='land'),
+    path('', login, name='land'),  # Landing page
     path('signup/', signup, name='signup'),
     path('login/', login, name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('rewards/', rewards, name='rewards'),
     path('supervisorrewards/', supervisorrewards, name='supervisorrewards'),
     path('add_reward/', add_reward, name='add_reward'),
-    path('action/', action, name = 'action'),
-    path('supervisor/', supervisor, name = 'supervisor'),
-    path('base/', base, name = 'base'),
-    path('landing/', landing, name = 'landing'),
-    path('campaign_form/', campaign_form, name = 'campaign_form'),
-    path('profile_setup/', profile_setup, name = 'profile_setup'),
+    path('action/', action, name='action'),
+    path('supervisor/', supervisor, name='supervisor'),
+    path('base/', base, name='base'),
+    path('landing/', landing, name='landing'),
+    path('campaign_form/', campaign_form, name='campaign_form'),
+    path('profile_setup/', profile_setup, name='profile_setup'),
     path('manage-supervisors/', manage_supervisors, name='manage_supervisors'),
     path('campaign/delete/<int:campaign_id>/', delete_campaign, name='delete_campaign'),
-    path('auth/', include('social_django.urls', namespace='social2')),
+    path('auth/', include('allauth.urls')), 
+    path('admin/', admin.site.urls), # Django Admin
+    path('accounts/', include('allauth.urls')),# Allauth URLs
 ]
