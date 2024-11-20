0250e3b96dd66d393ae2eacc72dd8d910e8a4f25
omeryurekli <omeryurekli2004@gmail.com>
2024-11-20 15:47:08 -0500

    Fixing User model
    so when we press login with google it takes us to profile setup now, that is bad but its a start?

--- bcsustain/main/models.py
+++ bcsustain/main/models.py
@@ -93,6 +93,9 @@ class Profile(models.Model):
 
     def __str__(self):
          return f"{self.user.username}'s Profile"
+    
+    def is_complete(self):
+        return self.google_username and self.google_email and self.graduation_year is not None
 
 @receiver(post_save,sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
