65554a9d06a655f995499c5f10823f0a737bb181
jacobhsi2h <hsiehjg@bc.edu>
2024-12-02 21:51:27 -0500

    fixed mess up

--- bcsustain/main/urls.py
+++ bcsustain/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import (
     signup, login, rewards, supervisorrewards, add_reward, action, base, 
     supervisor, landing, campaign_form, profile_setup, manage_supervisors, 
-    delete_campaign, redeem_reward, logout_view, delete_rewards
+    delete_campaign, redeem_reward, logout_view, delete_rewards, complete_campaign
 )
 from django.contrib.auth.views import LogoutView
 from django.contrib import admin
@@ -31,4 +31,5 @@ urlpatterns = [
     path('admin/', admin.site.urls), # Django Admin
     path('accounts/', include('allauth.urls')),# Allauth URLs
     path('rewards/add/', add_reward, name='add_reward'),
+    path('complete_campaign/<int:campaign_id>/', complete_campaign, name='complete_campaign'), 
 ]
